c87933ed2ee2ffe12c12e6d9fa0f132c3f1154d7
Pr0C33D <hero1393@gmail.com>
2021-10-05 21:37:20 +0300

    end of task 9
    end of task 9

--- exercises/09/js/script.js
+++ exercises/09/js/script.js
@@ -3,27 +3,43 @@ const plusDOM = document.querySelector('.plus');
 const resetDOM = document.querySelector('.reset');
 const numberDOM = document.querySelector('.number');
 const h1DOM = document.querySelector('h1');
+h1DOM.innerText = "Tu pasileidai zaidima - sekmes!";
 const defaultH1 = h1DOM.innerText;
 const swapTextOnClick = "Zaidimas progrese";
+const ulDOM = document.querySelector('ul');
 
+let button;
 let pressCounter = 0;
+
+function appendNode(){
+    const node = document.createElement("LI");
+    const textnode = document.createTextNode(`Paspaudei ${button} ir dabartinis rezultatas yra ` + pressCounter);
+    node.appendChild(textnode);
+    ulDOM.appendChild(node);
+}
 
 function minus() {
     pressCounter--;
     numberDOM.innerText = pressCounter;
     h1DOM.innerText = swapTextOnClick;
+    button = "Minusą";
+    appendNode();
 }
 
 function plus() {
     pressCounter++;
     numberDOM.innerText = pressCounter;
     h1DOM.innerText = swapTextOnClick;
+    button = "Pliusą";
+    appendNode();
 }
 
 function resetNumber() {
     pressCounter = 0;
     numberDOM.innerText = pressCounter;
     h1DOM.innerText = defaultH1;
+    button = "RESET"
+    appendNode();
 }
 
 minusDOM.addEventListener('click', minus);
